b3a2c89c450cab0c30bac87c7bb725b2347227fe
Abu Sayed <abus04651@gmail.com>
2024-10-20 22:41:36 +0600

    project initialize

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (       
+  <>
+    <h1 className={styles.grt}>Hi Welcome to MrPie websites</h1>
+  </>
+  );
+}
